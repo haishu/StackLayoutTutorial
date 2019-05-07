7b5be92368a423c848be2fb838c62a490ccb7cda
Haishu <haishu@live.com>
2019-05-08 01:48:17 +0200

    Initial check-in of module StackLayoutTutorial

---
+++ StackLayoutTutorial/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackLayoutTutorial
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            Console.WriteLine("OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+            Console.WriteLine("OnSleep");
+
+        }
+
+        protected override void OnResume()
+        {
+            Console.WriteLine("OnResume");
+        }
+    }
+}
